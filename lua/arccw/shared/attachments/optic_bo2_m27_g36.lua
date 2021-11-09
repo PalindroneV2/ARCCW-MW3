607e21d10419e7bd1e95a321fefa8a3af18b15fb
PalindroneV2 <asfadasfag@gmail.com>
2021-11-08 20:53:00 -0700

    acr and cm901 textures

---
+++ lua/arccw/shared/attachments/optic_bo2_m27_g36.lua
@@ -0,0 +1,12 @@
+att.PrintName = "Alternative Iron Sights"
+att.AbbrevName = "Alt. Iron Sights"
+att.Icon = Material("entities/acwatt_optic_bo1_irons.png", "mips smooth")
+att.Description = "You will aim with sights of iron, and you will like it."
+
+
+att.Desc_Neutrals = {"bo.desc"}
+att.SortOrder = 101
+att.Free = true
+
+att.Slot = "bo2_m27_irons"
+att.GivesFlags = {"g36_irons"}
