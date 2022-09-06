e8e9d8bd8381d06a65f1368f885e4a5cca8d3bb7
PalindroneV2 <asfadasfag@gmail.com>
2022-09-06 04:56:58 -0700

    blast shield and juggernaut working proper

--- lua/arccw/shared/attachments/mw3_perk_blastshield.lua
+++ lua/arccw/shared/attachments/mw3_perk_blastshield.lua
@@ -1,23 +1,22 @@
 att.PrintName = "Blast Shield"
 att.Icon = Material("entities/mw3_ui/perk_blastshield.png", "mips smooth")
-att.Description = "Reduces explosive damage by 50%."
+att.Description = "Reduces explosive damage by 40%."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
 }
 att.Slot = {"mw3_perk"}
 att.AutoStats = true
-att.Hidden = true
+att.Hidden = false
 
 att.MW3_BlastShield = true
 
-/*
-hook.Add("EntityTakeDamage", "ArcCW_MW3_BlastShield", function(ply, dmg)
-    local wep = ply:GetActiveWeapon()
-    if not IsValid(wep) or not wep.ArcCW or not wep:GetBuff_Override("MW3_BlastShield") then return end
+hook.Add("EntityTakeDamage", "ArcCW_MW3_BlastShield", function(ent, dmg)
+    if !(ent:IsPlayer() or ent:IsNPC()) then return end
+    local wep = ent:GetActiveWeapon()
+    if !IsValid(wep) or !wep.ArcCW or !wep:GetBuff_Override("MW3_BlastShield") then return end
 
     if wep:GetBuff_Override("MW3_BlastShield") and dmg:GetDamageType() == DMG_BLAST then
-        dmg:ScaleDamage(0.5)
+        dmg:ScaleDamage(0.6)
     end
 end)
-*/
